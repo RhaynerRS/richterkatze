155c210019030ee2905b936b3073e8a27a986532
RhaynerRS <80178543+RhaynerRS@users.noreply.github.com>
2021-03-07 00:06:18 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ function Home(){
             <meta charset="UTF-8"/>
             <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <link rel="icon" type="imagem/png" href="https://i.imgur.com/iCe4xZP.png" />
+            
             <title>Richterkatze Studios</title>
             <script src="https://kit.fontawesome.com/82464d366f.js" crossorigin="anonymous"></script>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
